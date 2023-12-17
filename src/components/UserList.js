9a87a4b69c23c79b95394701c2619212cddf0786
Varun Singh <104829215+VarunSingh19@users.noreply.github.com>
2023-12-17 16:16:57 +0530

    Done

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import loader from '../loader/loadergif.gif'
+import loader from '../loader/loadergif.gif';
 import UserProfile from './UserProfile';
 
 const UserList = () => {
@@ -10,20 +10,30 @@ const UserList = () => {
     const [selectedUser, setSelectedUser] = useState(null);
     const [showClickMessage, setShowClickMessage] = useState(true);
     const [imageLoading, setImageLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const usersPerPage = 10;
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await axios.get('https://602e7c2c4410730017c50b9d.mockapi.io/users?');
-                setUsers(response.data);
+                const response = await axios.get('https://602e7c2c4410730017c50b9d.mockapi.io/users');
+                const uniqueIds = new Set();
+                const filteredUsers = response.data.filter(user => {
+                    if (!uniqueIds.has(user.id)) {
+                        uniqueIds.add(user.id);
+                        return true;
+                    }
+                    return false;
+                });
+                setUsers(filteredUsers);
                 setLoading(false);
-                console.log(response.data)
+                console.log(filteredUsers);
             } catch (error) {
                 console.error('Error fetching data:', error);
                 setLoading(false);
             }
         };
-
+        
         fetchUsers();
     }, []);
 
@@ -45,14 +55,22 @@ const UserList = () => {
     const handleImageError = () => {
         setImageLoading(false);
     };
+
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
     return (
-        <div className="container  mt-4 text-white" >
+        <div className="container mt-4 text-white">
             <div className='container p-2 text-center' style={{ backgroundColor: 'black', border: '2px solid', borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }}>
                 <div onClick={toggleUserList} style={{ cursor: 'pointer' }} className='h2 mx-auto'>User List</div>
             </div>
             {showClickMessage && (
-                <div className='text-center  ' style={{ marginTop: '50px' }} >
-
+                <div className='text-center' style={{ marginTop: '50px' }}>
                     <div style={{ color: 'black' }} className='p-3 '>
                         <p>Click on User List Please!</p>
                     </div>
@@ -60,11 +78,10 @@ const UserList = () => {
             )}
             {showUserList && (
                 <div className="row">
-                    <div className="col-md-6  " >
-                        <ul className="list-group " >
+                    <div className="col-md-6">
+                        <ul className="list-group">
                             {loading && <p>Loading...</p>}
-                            {!loading && users.length === 0 && <p>No data to show</p>}
-                            {!loading && users.slice(1, 11).map(user => (
+                            {!loading && currentUsers.map(user => (
                                 <li
                                     key={user.id}
                                     className={`list-group-item shadow ${selectedUser === user ? 'active' : ''}`}
@@ -77,8 +94,8 @@ const UserList = () => {
                                             alt={user.profile.username}
                                             className="mr-3 rounded-circle shadow bg-white"
                                             style={{ width: '50px', height: '50px' }}
-                                            onError={handleImageError} 
-                                            onLoad={() => setImageLoading(false)}
+                                            onError={handleImageError}
+                                            onLoad={handleImageLoaded}
                                         />
                                         <div>
                                             <div style={{ marginLeft: '20px' }}>
@@ -89,13 +106,23 @@ const UserList = () => {
                                 </li>
                             ))}
                         </ul>
+                       {/* Pagination */}
+<nav className="mt-3 mx-auto">
+    <ul className="pagination">
+        {Array.from({ length: Math.ceil(users.length / usersPerPage) }).map((_, index) => (
+            <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                <button className="page-link" onClick={() => paginate(index + 1)}>{index + 1}</button>
+            </li>
+        ))}
+    </ul>
+</nav>
+
                     </div>
                     <div className="col-md-6 text-center">
                         {selectedUser ? (
                             <UserProfile user={selectedUser} />
                         ) : (
-                            <div className='text-center  rounded' style={{ marginTop: '50px' }} >
-
+                            <div className='text-center  rounded' style={{ marginTop: '50px' }}>
                                 <div style={{ color: 'black' }} className='p-3 shadow'>
                                     <p>No user is selected yet...</p>
                                 </div>
